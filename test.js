2256178a6947a6fe5dde22ac53c36e180a62baa3
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-04-01 17:45:50 -0700

    Fix tests for HTTP redirects

--- test.js
+++ test.js
@@ -1,23 +1,33 @@
-var request = require("then-request");
+var request = require("request-promise");
 
 module.exports = function test(config) {
-  var errors = [];
+  var errors = 0;
   var chain = Object.keys(config.urls).reduce(
     function(p, buildId) {
       var url = config.urls[buildId];
       return p.then(function() {
-        return request("HEAD", url).catch(function(err) {
-          errors.push("Failed to fetch " + url + " " + err.message);
-        });
+        return request({
+          method: "GET",
+          uri: url,
+          resolveWithFullResponse: true
+        })
+          .then(function(response) {
+            if (response.statusCode != 200) {
+              throw new Error("Status code " + response.statusCode);
+            }
+            console.log("OKAY: " + url);
+          })
+          .catch(function(err) {
+            console.error("  - Failed to fetch " + url + " " + err.message);
+            errors += 1;
+          });
       });
     },
     Promise.resolve()
   );
   return chain.then(function() {
-    if (errors.length > 0) {
-      var output = "There were errors when validating your published packages:\n\n  - " +
-        errors.join("\n\n  - ") +
-        "\n";
+    if (errors > 0) {
+      var output = "There were errors when validating your published packages\n";
       throw new Error(output);
     }
   });
