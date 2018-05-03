067b6013d04241d58215cd21ea5012a467f98690
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-05-03 12:09:27 -0700

    Include linux binary in test_app

--- test_app/index.js
+++ test_app/index.js
@@ -4,6 +4,7 @@ module.exports = binwrap({
   binaries: ["echoMe"],
   urls: {
     "darwin-x64": "http://localhost:9999/echoMe-0.0.0-mac-x64.tgz",
+    "linux-x64": "http://localhost:9999/echoMe-0.0.0-linux-x64.tgz",
     "win32-x64": "http://localhost:9999/echoMe-0.0.0-win-i386.zip"
   }
 });
