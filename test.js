95cd338a46a8bf381eb64aff1194d343d9237453
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-06-11 21:30:17 -0700

    Download each binary at most once when running binwrap-test

--- test.js
+++ test.js
@@ -2,26 +2,34 @@ var request = require("request-promise");
 
 module.exports = function test(config) {
   var errors = 0;
+  var checkedUrls = {};
+
   var chain = Object.keys(config.urls).reduce(
     function(p, buildId) {
       var url = config.urls[buildId];
-      return p.then(function() {
-        return request({
-          method: "GET",
-          uri: url,
-          resolveWithFullResponse: true
-        })
-          .then(function(response) {
-            if (response.statusCode != 200) {
-              throw new Error("Status code " + response.statusCode);
-            }
-            console.log("OKAY: " + url);
+      if (checkedUrls[url]) {
+        return p.then(function() {
+          console.log("OKAY: [" + buildId + "] " + url);
+        });
+      } else {
+        return p.then(function() {
+          return request({
+            method: "GET",
+            uri: url,
+            resolveWithFullResponse: true
           })
-          .catch(function(err) {
-            console.error("  - Failed to fetch " + url + " " + err.message);
-            errors += 1;
-          });
-      });
+            .then(function(response) {
+              if (response.statusCode != 200) {
+                throw new Error("Status code " + response.statusCode);
+              }
+              console.log("OKAY: " + url);
+            })
+            .catch(function(err) {
+              console.error("  - Failed to fetch " + url + " " + err.message);
+              errors += 1;
+            });
+        });
+      }
     },
     Promise.resolve()
   );
